3bd2b5ec98dce633902ce119f789680039e40435
Stucki <loganstucki@gmail.com>
2019-01-09 15:22:05 -0600

    Updates

---
+++ precache-manifest.f5c375c1d35cb40cb806f5e91c93a0dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d553cd39b35021989d",
+    "url": "/sensortag-react-app/static/js/runtime~main.c6d553cd.js"
+  },
+  {
+    "revision": "2ff682c7ea4ebb5da4e8",
+    "url": "/sensortag-react-app/static/js/main.2ff682c7.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/sensortag-react-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "2ff682c7ea4ebb5da4e8",
+    "url": "/sensortag-react-app/static/css/main.f83f7e22.chunk.css"
+  },
+  {
+    "revision": "879109763d2b1000bef04a3f48f2fb77",
+    "url": "/sensortag-react-app/index.html"
+  }
+];
